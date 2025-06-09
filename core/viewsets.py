8359515125381804603ad8bd0a228c27a4454ae5
BBolatA <neuralnetnimble@gmail.com>
2025-06-09 15:50:31 +0500

    fix: softdelete

--- core/viewsets.py
+++ core/viewsets.py
@@ -2,21 +2,30 @@ from django.http import Http404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
+from cashflow.models import CashFlow
+from cashflow.serializers import CashFlowSerializer
+
 
 class SoftDeleteModelViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         lookup = self.lookup_field
-        lookup_val = self.kwargs.get(self.lookup_url_kwarg or lookup)
+        lookup_val = kwargs.get(self.lookup_url_kwarg or lookup)
 
-        Model = self.queryset.model
+        qs = self.get_queryset()
+        Model = qs.model
 
         try:
-            instance = self.get_queryset().get(**{lookup: lookup_val})
-        except Http404:
+            instance = qs.get(**{lookup: lookup_val})
+        except Model.DoesNotExist:
             try:
-                Model.all_objects.get(**{lookup: lookup_val})
+                deleted = Model.all_objects.get(**{lookup: lookup_val})
+                return Response(status=status.HTTP_204_NO_CONTENT)
             except Model.DoesNotExist:
-                raise
-            return Response(status=status.HTTP_204_NO_CONTENT)
+                raise Http404
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CashFlowViewSet(SoftDeleteModelViewSet):
+    queryset = CashFlow.objects.all()
+    serializer_class = CashFlowSerializer
